18d2722fad1f3ccf3dfeb02bfffd9a3bc426e29c
ShSato4 <ShSato4.private.1221@gmail.com>
2021-09-15 00:26:10 +0900

    add sample source

---
+++ PDFManager/PDFManager/MergePDFs.vb
@@ -0,0 +1,63 @@
+﻿'
+' https://zero0nine.com/archives/2866 のソースをVB.net に書き換えただけ。
+'
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+Module MergePDFs
+
+    Sub Main()
+        Debug.WriteLine("MergePDFs start")
+
+        Dim pdfsDir As String = ".\..\..\PDFs"
+        Dim outSrcName As String = "Merge.pdf"
+        Dim outSrcDir As String = ".\output"
+        Dim outSrcPath As String = $"{outSrcDir}\{outSrcName}"
+
+        If Not Directory.Exists(outSrcDir) Then
+            Directory.CreateDirectory(outSrcDir)
+        End If
+
+        If File.Exists(outSrcPath) Then
+            File.Delete(outSrcPath)
+        End If
+
+        ' PDFファイルを取得
+        Dim files As IEnumerable(Of String) = Directory.EnumerateFiles(pdfsDir, "*.pdf", SearchOption.TopDirectoryOnly)
+
+        ' PDF集約
+        Dim doc As Document = Nothing
+        Dim pdfCopy As PdfCopy = Nothing
+
+        Try
+            doc = New Document()
+            pdfCopy = New PdfCopy(doc, New FileStream(outSrcPath, FileMode.Create))
+
+            doc.Open()
+
+            ' 出力するPDFのプロパティを設定(必要なさそう)
+            doc.AddKeywords("a")
+            doc.AddAuthor("b")
+            doc.AddTitle("c")
+            doc.AddCreator("d")
+            doc.AddSubject("e")
+
+            For Each file In files
+                Dim pdfReader As New PdfReader(file)
+                pdfCopy.AddDocument(pdfReader)
+                pdfReader.Close()
+            Next
+
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+
+        Finally
+            doc.Close()
+            pdfCopy.Close()
+
+        End Try
+
+        Debug.WriteLine("MergePDFs end")
+    End Sub
+
+End Module
---
+++ PDFManager/PDFManager/RemovePages.vb
@@ -0,0 +1,34 @@
+﻿'
+' https://ktts.hatenablog.com/entry/2018/07/17/013829 のソースをVB.net に書き換えただけ。
+'
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+Module RemovePages
+
+    Sub Main()
+        Debug.WriteLine("RemovePages start")
+
+        Dim targetSrcPath As String = ".\output\Merge.pdf"
+
+        Dim outSrcName As String = "RemovePages.pdf"
+        Dim outSrcDir As String = ".\output"
+        Dim outSrcPath As String = $"{outSrcDir}\{outSrcName}"
+
+        Try
+            Using pdfReader As New PdfReader(targetSrcPath)
+                pdfReader.SelectPages("1,3")
+                Using pdfStamper As New PdfStamper(pdfReader, New FileStream(outSrcPath, FileMode.Create))
+                    Debug.WriteLine("delete success!")
+                End Using
+            End Using
+
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+
+        End Try
+
+        Debug.WriteLine("RemovePages end")
+    End Sub
+
+End Module
